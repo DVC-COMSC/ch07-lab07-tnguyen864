391de506e362d234a1c2679ef34fc00e4c331592
Thuy Nguyen <thuicide@gmail.com>
2022-10-10 03:34:53 -0700

    Chap7 Lab7

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-
+# Create list and get 10 values for list
 inputvalues = input('Enter all elements values: ')
 numbers1 = inputvalues.split() 
-for i in range(len(numbers1)):
+for i in range(10):
 	numbers1[i] = int(numbers1[i]) 
-# The following line is the same as the for-loop
-# numbers1 = list(map(int, numbers))
 
-# print ("The original list: ", numbers1)
+# Print original list
+print("The original list:", numbers1)
 
-# ******************************
-# Make your Code
-# ******************************
+# New list for even numbers
+evenList = []
+
+# Get even numbers from list
+evenList.append(numbers1.pop(0))
+evenList.append(numbers1.pop(1))
+evenList.append(numbers1.pop(2))
+evenList.append(numbers1.pop(3))
+evenList.append(numbers1.pop(4))
+
+# Print updated list and even list
+print("The list numbers now: ", numbers1)
+print("The list for even index elements:", evenList)
